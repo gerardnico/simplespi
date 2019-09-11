5daea6cfc12429e561737267623c6639c2df7e8a
Nicolas GERARD <gerardnico@gmail.com>
2019-09-11 22:59:31 +0200

    backup staged changes

--- src/work/WorkProvider.java
+++ src/work/WorkProvider.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 2007, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 
 package work;
 
